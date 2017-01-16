cec90e9e6017c2f59a158ac99795766658629754
joaorodriguesjr <mail@joaorodriguesjr.com>
2017-01-16 17:41:16 -0200

    Register the Serializer service

--- web/middlewares.php
+++ web/middlewares.php
@@ -1,6 +1,7 @@
 <?php
 
 $app->register(new Silex\Provider\ServiceControllerServiceProvider());
+$app->register(new Silex\Provider\SerializerServiceProvider());
 
 $app->get('/v1/lists', 'lists.resource.handler.v1:get');
 $app->get('/v1/lists/{list}/items', 'items.resource.handler.v1:get');
