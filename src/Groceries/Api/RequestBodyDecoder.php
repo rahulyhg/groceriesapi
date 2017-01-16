6a735171b5072a6cfd2d31078df7ba07d5357940
joaorodriguesjr <mail@joaorodriguesjr.com>
2017-01-16 19:03:32 -0200

    Add RequestBodyDecoder middleware

--- src/Groceries/Api/RequestBodyDecoder.php
+++ src/Groceries/Api/RequestBodyDecoder.php
@@ -2,6 +2,7 @@
 
 namespace Groceries\Api;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Serializer\Serializer;
 
 class RequestBodyDecoder
@@ -11,5 +12,22 @@ class RequestBodyDecoder
     public function __construct(Serializer $serializer)
     {
         $this->serializer = $serializer;
+    }
+
+    public function __invoke(Request $request)
+    {
+        $data = null;
+
+        if ($request->headers->get('Content-Type') === 'application/json') {
+            $data = $this->serializer->decode($request->getContent(), 'json');
+        }
+
+        if ($request->headers->get('Content-Type') === 'application/xml') {
+            $data = $this->serializer->decode($request->getContent(), 'xml');
+        }
+
+        if (is_array($data)) {
+            $request->request->replace($data);
+        }
     }
 }
--- web/middlewares.php
+++ web/middlewares.php
@@ -3,5 +3,7 @@
 $app->register(new Silex\Provider\ServiceControllerServiceProvider());
 $app->register(new Silex\Provider\SerializerServiceProvider());
 
+$app->before('request.body.decoder');
+
 $app->get('/v1/lists', 'lists.resource.handler.v1:get');
 $app->get('/v1/lists/{list}/items', 'items.resource.handler.v1:get');
