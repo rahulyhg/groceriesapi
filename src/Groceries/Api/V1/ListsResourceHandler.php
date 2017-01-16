d4af863f7efd8ad9ceaa0bac34ce6f5ec1a50bc4
joaorodriguesjr <mail@joaorodriguesjr.com>
2017-01-16 12:58:10 -0200

    Add Request handler method

--- src/Groceries/Api/V1/ListsResourceHandler.php
+++ src/Groceries/Api/V1/ListsResourceHandler.php
@@ -2,6 +2,9 @@
 
 namespace Groceries\Api\V1;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
 use Groceries\Lists\DataAccess;
 
 class ListsResourceHandler
@@ -11,5 +14,18 @@ class ListsResourceHandler
     public function __construct(DataAccess $dataAccess)
     {
         $this->dataAccess = $dataAccess;
+    }
+
+    public function get(Request $request)
+    {
+        $month = filter_var($request->query->get('month'), FILTER_SANITIZE_STRING);
+        $year  = filter_var($request->query->get('year'),  FILTER_SANITIZE_STRING);
+
+        if (! $month || ! $year) {
+            return new JsonResponse(['error' => 'requires parameters month and year'], 400);
+        }
+
+        $data = $this->dataAccess->getListsByMonth($month, $year);
+        return new JsonResponse($data);
     }
 }
