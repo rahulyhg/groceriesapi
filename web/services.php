f70a7960648e11d22bcaebfaa7d22e350fbea5c5
joaorodriguesjr <mail@joaorodriguesjr.com>
2017-01-16 12:17:15 -0200

    Add PDO connection as service

--- web/services.php
+++ web/services.php
@@ -1,1 +1,9 @@
 <?php
+
+$app['db.connection'] = function () {
+    $connection = new PDO(getenv('DATABASE_DSN'), getenv('DATABASE_USER'), getenv('DATABASE_PASS'));
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    return $connection;
+};
