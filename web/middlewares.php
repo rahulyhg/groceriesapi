e46fb6866b32e61ddd6cb201c33b14b4d8dad966
joaorodriguesjr <mail@joaorodriguesjr.com>
2017-01-18 20:59:50 -0200

    Request authentication must happen before

--- web/middlewares.php
+++ web/middlewares.php
@@ -6,11 +6,11 @@ $app->register(new Silex\Provider\SerializerServiceProvider());
 $app->before('request.body.decoder');
 
 $app->get('/v1/token', 'token.resource.handler.v1:get');
-$app->get('/v1/lists', 'lists.resource.handler.v1:get')->after('request.authentication');
-$app->get('/v1/items', 'items.resource.handler.v1:get')->after('request.authentication');
+$app->get('/v1/lists', 'lists.resource.handler.v1:get')->before('request.authentication');
+$app->get('/v1/items', 'items.resource.handler.v1:get')->before('request.authentication');
 
-$app->post('/v1/lists', 'lists.resource.handler.v1:post')->after('request.authentication');
-$app->post('/v1/items', 'items.resource.handler.v1:post')->after('request.authentication');
+$app->post('/v1/lists', 'lists.resource.handler.v1:post')->before('request.authentication');
+$app->post('/v1/items', 'items.resource.handler.v1:post')->before('request.authentication');
 
-$app->delete('/v1/lists/{id}', 'lists.resource.handler.v1:delete')->after('request.authentication');
-$app->delete('/v1/items/{id}', 'items.resource.handler.v1:delete')->after('request.authentication');
+$app->delete('/v1/lists/{id}', 'lists.resource.handler.v1:delete')->before('request.authentication');
+$app->delete('/v1/items/{id}', 'items.resource.handler.v1:delete')->before('request.authentication');
