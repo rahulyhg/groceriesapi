594542d85073f568e9fe4a69173fe93b8a1f0c43
joaorodriguesjr <mail@joaorodriguesjr.com>
2017-01-18 22:34:42 -0200

    Remove early JSON encoding

--- src/Groceries/Api/V1/ListsResourceHandler.php
+++ src/Groceries/Api/V1/ListsResourceHandler.php
@@ -3,7 +3,7 @@
 namespace Groceries\Api\V1;
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 use Groceries\Lists\DataAccess;
 use Groceries\Api\UuidGenerator;
@@ -22,38 +22,49 @@ class ListsResourceHandler
 
     public function get(Request $request)
     {
+        $status = 400;
+        $data = ['error' => 'requires parameters month and year'];
+
         $month = filter_var($request->query->get('month'), FILTER_SANITIZE_STRING);
         $year  = filter_var($request->query->get('year'),  FILTER_SANITIZE_STRING);
 
-        if (! $month || ! $year) {
-            return new JsonResponse(['error' => 'requires parameters month and year'], 400);
+        if ($month && $year) {
+            $status = 200;
+            $data = $this->dataAccess->getListsByMonth($month, $year);
         }
 
-        $data = $this->dataAccess->getListsByMonth($month, $year);
-        return new JsonResponse($data);
+        return new Response(serialize($data), $status);
     }
 
     public function post(Request $request)
     {
+        $status = 400;
+        $data = ['error' => 'requires date format YYYY-MM-DD'];
+
         $date = filter_var($request->request->get('date'), FILTER_SANITIZE_STRING);
 
-        if (! preg_match('#^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$#', $date)) {
-            return new JsonResponse(['error' => 'requires date format YYYY-MM-DD'], 400);
+        if (preg_match('#^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$#', $date)) {
+            $status = 201;
+            $data = ['id' => $this->uuidGenerator->generate(), 'date' => $date];
+
+            $this->dataAccess->createList($data);
         }
 
-        $data = ['id' => $this->uuidGenerator->generate(), 'date' => $date];
-        $this->dataAccess->createList($data);
-
-        return new JsonResponse($data, 201);
+        return new Response(serialize($data), $status);
     }
 
     public function delete(string $id)
     {
-        if (! $this->dataAccess->getListByID($id)) {
-            return new JsonResponse(['error' => 'list not found'], 404);
+        $status = 404;
+        $data = ['error' => 'list not found'];
+
+        if ($this->dataAccess->getListByID($id)) {
+            $status = 200;
+            $data = ['message' => 'list deleted'];
+
+            $this->dataAccess->deleteList($id);
         }
 
-        $this->dataAccess->deleteList($id);
-        return new JsonResponse();
+        return new Response(serialize($data), $status);
     }
 }
